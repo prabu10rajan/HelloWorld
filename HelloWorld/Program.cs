474ce775e833ea3fe4b1044bc7f9d9107796ca51
Prabu Rajan <prabu10rajan@gmail.com>
2019-08-31 14:51:39 +0530

    Missed the gitignore file

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing Git Bash");
+            Console.WriteLine("Using GitBash to push code into github");
+            Console.ReadLine();
+        }
+    }
+}
